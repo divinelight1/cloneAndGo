4bae0827ae0d7f233a2e031f71f381798bc22003
Aldin Mujkic <aldinmujkic44@gmail.com>
2018-01-19 19:54:55 +0100

    add footer and header default code

---
+++ footer.php
@@ -0,0 +1,8 @@
+<script src="<?php bloginfo('template_directory'); ?>js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src="<?php bloginfo('template_directory'); ?>js/plugins.js"></script>
+        <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
+        <!-- bootstrap js cdn -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title><?php bloginfo('name'); ?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <!-- default style files -->
+        <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/normalize.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/main.css">
+        <!-- bootstrap cdn-->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        
